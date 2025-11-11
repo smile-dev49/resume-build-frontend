7992ab4c2ff1790890b897bdaf4cc32e5817c2ec
orllovihor-cpu <orllovihor@gmail.com>
2025-11-12 06:07:52 +0900

    changed input tyep of email

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -57,7 +57,7 @@ const LoginPage = () => {
               </label>
               <input
                 id="username"
-                type="email"
+                type="text"
                 autoComplete="username"
                 className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
                 placeholder="Email address"
