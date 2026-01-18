e0acac03bd0edf789acecdbeda426d543307b431
smile-dev49 <nancyspace89@gmail.com>
2026-01-18 10:24:01 -0500

    updated auth

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -17,8 +17,8 @@ const LoginPage = () => {
 
   const { register, handleSubmit, formState: { errors } } = useForm<LoginForm>({
     defaultValues: {
-      username: 'rusuland9@gmail.com',
-      password: '20030807',
+      username: '',
+      password: '',
     },
   })
 
